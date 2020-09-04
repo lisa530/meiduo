01ae54b120aa55bdc6d42b3afaf094e6bc707c41
lisa <lisa_283022177@126.com>
2020-09-04 15:29:59 +0800

    用户名注册： 手机号是否重复注册前端实现

--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -90,6 +90,27 @@ let vm = new Vue({
             this.error_mobile_message = '您输入的手机号格式不正确';
             this.error_mobile = true;
             }
+
+        // 判断手机号是否重复注册
+        if (this.error_mobile == false){
+            let url = '/mobiles/' + this.mobile + '/count/';
+            axios.get(url,{
+                responseType: 'json'
+            })
+                .then(response =>{
+                // 手机号存在 将error_mobile的值改为 true
+                if (response.data.count == 1){
+                    this.error_mobile_message = '手机号已存在';
+                    this.error_mobile = true;
+                }else{
+                    this.error_mobile = false;
+                }
+
+             })
+                .catch(error =>{
+                    console.log(error.response)
+            })
+        }
         },
         // 校验是否勾选协议
         check_allow(){

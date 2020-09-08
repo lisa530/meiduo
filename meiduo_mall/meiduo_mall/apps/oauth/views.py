042b9c07588865ea3001444ac795c043b444e00e
lisa <lisa_283022177@126.com>
2020-09-08 10:50:24 +0800

    用户登录之QQ登录：处理QQ登录回调(提取code,通过code获取access_token,通过access_token获取openid)

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -5,8 +5,38 @@ from django.conf import settings
 from django import http
 
 from meiduo_mall.utils.response_code import RETCODE
+import logging
 
-# Create your views here.
+
+# 创建日志输出器
+logger = logging.getLogger('django')
+
+
+class QQAuthUserView(View):
+    """处理QQ登录回调地址 oauth_callback"""
+
+    def get(self,request):
+        """处理QQ登录回调的业务逻辑"""
+
+        # 1.获取code参数
+        code = request.GET.get('code')
+        if not code:
+            return http.HttpResponseForbidden('获取code失败')
+
+        # 2.创建工具对象
+        oauth = oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID, client_secret=settings.QQ_CLIENT_SECRET,
+                            redirect_uri=settings.QQ_REDIRECT_URI)
+
+        try:
+            # 使用code获取access_token
+            access_token = oauth.get_access_token(code)
+            # 使用access_token获取openid
+            openid = oauth.get_open_id(access_token)
+        except Exception as e:
+            logger.error(e)
+            return http.HttpResponseServerError('OAuth2.0认证失败')
+        pass
+
 
 class QQAuthURLView(View):
     """提供QQ扫码登录页面"""

22e6b2e9c01c5614f5052f26cdac22f15b1a7b7e
lisa <lisa_283022177@126.com>
2020-09-09 19:40:17 +0800

    补充使用celery发送邮件错误重试次数

--- meiduo_mall/celery_tasks/email/tasks.py
+++ meiduo_mall/celery_tasks/email/tasks.py
@@ -2,9 +2,13 @@ from django.core.mail import send_mail
 from django.conf import settings
 from celery_tasks.main import celery_app
 
-
-@celery_app.task(name='send_verify_email')
-def send_verify_email(to_email, verify_url):
+# @celery_app.task(name='send_verify_email')
+# bind：保证task对象会作为第一个参数自动传入
+# name：异步任务别名
+# retry_backoff：异常自动重试的时间间隔 第n次(retry_backoff×2^(n-1))s
+# max_retries：异常自动重试次数的上限
+@celery_app.task(bind=True,name='send_verify_email',retry_backoff=3)
+def send_verify_email(self,to_email, verify_url):
     # send_mail('标题', '普通邮件正文', '发件人', '收件人列表', '富文本邮件正文(html)')
     """
         定义发送邮件的任务
@@ -19,4 +23,8 @@ def send_verify_email(to_email, verify_url):
                    '<p>您的邮箱为：%s 。请点击此链接激活您的邮箱：</p>' \
                    '<p><a href="%s">%s<a></p>' % (to_email, verify_url, verify_url)
 
-    send_mail(subject, "", settings.EMAIL_FROM, [to_email], html_message=html_message)
+    try:
+        send_mail(subject, "", settings.EMAIL_FROM, [to_email], html_message=html_message)
+    except Exception as e:
+        # 最多重试3次
+        raise self.retry(exec=e, max=3)
--- meiduo_mall/celery_tasks/sms/tasks.py
+++ meiduo_mall/celery_tasks/sms/tasks.py
@@ -3,9 +3,13 @@ from celery_tasks.sms.yuntongxun.ccp_sms import CCP
 from celery_tasks.sms import constants
 from celery_tasks.main import celery_app
 
+# bind：保证task对象会作为第一个参数自动传入
+# name：异步任务别名
+# retry_backoff：异常自动重试的时间间隔 第n次(retry_backoff×2^(n-1))s
+# max_retries：异常自动重试次数的上限
 # 使用装饰器装饰异步任务,给异步任务取一个别名保证celery能识别任务
-@celery_app.task(name='send_sms_code')
-def send_sms_code(mobile,sms_code):
+@celery_app.task(bind=True, name='send_sms_code', retry_backoff=3)
+def send_sms_code(self,mobile,sms_code):
     """
     发送短信验证码的异步任务
     :param mobile: 手机号
@@ -13,5 +17,12 @@ def send_sms_code(mobile,sms_code):
     :return: 成功：0 失败: -1
     """
     # mobile:接收短信手机号， sms_code短信  短信过期时间: 300秒 // 60(5分钟内有效) , 模板id
-    send_ret = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+    try:
+        send_ret = CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], constants.SEND_SMS_TEMPLATE_ID)
+    except Exception as e:
+        raise self.retry(exc=e,max_retries=3)
+    # 有异常自动重试三次
+    if send_ret != 0:
+        # 有异常自动重试三次
+        raise self.retry(exc=Exception('发送短信失败'), max_retries=3)
     return send_ret

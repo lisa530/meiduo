643f38eb120734c2bd0134e290e8c85f050e5155
lisa <lisa_283022177@126.com>
2020-09-19 17:00:42 +0800

    使用乐观锁下订单(修改后)

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -83,7 +83,7 @@ class OrderSuccessView(LoginRequiredMixin, View):
 
 
 class OrderCommitView(LoginRequiredJSONMixin, View):
-    """订单提交"""
+    """提交订单"""
 
     def post(self, request):
         """保存订单基本信息和订单商品信息"""
@@ -117,16 +117,15 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
                 order_id = timezone.localtime().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
                 # 保存订单基本信息（一）
                 order = OrderInfo.objects.create(
-                    order_id=order_id, # 订单id
-                    user=user, # 当前登录用户
-                    address=address,
-                    total_count=0,
-                    total_amount=Decimal(0.00), # 总金额
-                    freight=Decimal(10.00), # 运费
-                    pay_method=pay_method, # 支付方式
+                    order_id = order_id,
+                    user = user,
+                    address = address,
+                    total_count = 0,
+                    total_amount = Decimal(0.00),
+                    freight = Decimal(10.00),
+                    pay_method = pay_method,
                     # status = 'UNPAID' if pay_method=='ALIPAY' else 'UNSEND'
-                    status=OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM[
-                        'ALIPAY'] else OrderInfo.ORDER_STATUS_ENUM['UNSEND']
+                    status = OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM['ALIPAY'] else OrderInfo.ORDER_STATUS_ENUM['UNSEND']
                 )
 
                 # 保存订单商品信息（多）
@@ -140,51 +139,76 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
                 # 构造购物车中被勾选的商品的数据 {b'1': b'1'}
                 new_cart_dict = {}
                 for sku_id in redis_selected:
-                    # 取出购物车中所有数据, (sku_id count) 赋值给new_cart_dict
                     new_cart_dict[int(sku_id)] = int(redis_cart[sku_id])
 
                 # 获取被勾选的商品的sku_id
                 sku_ids = new_cart_dict.keys()
                 for sku_id in sku_ids:
-                    # 读取购物车商品信息
-                    sku = SKU.objects.get(id=sku_id)  # 查询商品和库存信息时，不能出现缓存，所以没用filter(id__in=sku_ids)
 
-                    # 获取要提交订单的商品的数量
-                    sku_count = new_cart_dict[sku.id]
-                    # 判断商品数量是否大于库存，如果大于，响应"库存不足"
-                    if sku_count > sku.stock:
-                        # 库存不足，回滚
-                        transaction.savepoint_rollback(save_id)
-                        return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
+                    # 每个商品都有多次下单的机会，直到库存不足
+                    while True:
+                        # 读取购物车商品信息
+                        sku = SKU.objects.get(id=sku_id) # 查询商品和库存信息时，不能出现缓存，所以没用filter(id__in=sku_ids)
 
-                    # SKU 减库存，加销量
-                    sku.stock -= sku_count
-                    sku.sales += sku_count
-                    sku.save()
+                        # 获取原始的库存和销量
+                        origin_stock = sku.stock
+                        origin_sales = sku.sales
 
-                    # SPU 加销量
-                    sku.spu.sales += sku_count  # 一查多: 一个sku对应多个spu
-                    sku.spu.save()
+                        # 获取要提交订单的商品的数量
+                        sku_count = new_cart_dict[sku.id]
+                        # 判断商品数量是否大于库存，如果大于，响应"库存不足"
+                        if sku_count > origin_stock:
+                            # 库存不足，回滚
+                            transaction.savepoint_rollback(save_id)
+                            return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
 
-                    # 保存订单商品信息
-                    OrderGoods.objects.create(
-                        order=order,  # 关联的订单对象
-                        sku=sku,
-                        count=sku_count,
-                        price=sku.price,
-                    )
+                        # 模拟网络延迟
+                        # import time
+                        # time.sleep(7)
 
-                    # 累加订单商品的数量和总价到订单基本信息表
-                    order.total_count += sku_count
-                    order.total_amount += sku_count * sku.price
+                        # SKU 减库存，加销量
+                        # sku.stock -= sku_count
+                        # sku.sales += sku_count
+                        # sku.save()
+
+                        new_stock = origin_stock - sku_count
+                        new_sales = origin_sales + sku_count
+                        result = SKU.objects.filter(id=sku_id, stock=origin_stock).update(stock=new_stock, sales=new_sales)
+                        # 如果在更新数据时，原始数据变化了，返回0；表示有资源抢夺
+                        if result == 0:
+                            # 库存 10，要买1，但是在下单时，有资源抢夺，被买走1，剩下9个，如果库存依然满足，继续下单，直到库存不足为止
+                            # return http.JsonResponse('下单失败')
+                            continue
+
+                        # SPU 加销量
+                        sku.spu.sales += sku_count
+                        sku.spu.save()
+
+                        OrderGoods.objects.create(
+                            order = order,
+                            sku = sku,
+                            count = sku_count,
+                            price = sku.price,
+                        )
+
+                        # 累加订单商品的数量和总价到订单基本信息表
+                        order.total_count += sku_count
+                        order.total_amount += sku_count * sku.price
+
+                        # 下单成功，记得break
+                        break
 
                 # 再加最后的运费
                 order.total_amount += order.freight
                 order.save()
-
-            except Exception as e:  # 数据库操作失败,回滚
+            except Exception as e:
                 transaction.savepoint_rollback(save_id)
                 return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '下单失败'})
+
+            # 数据库操作成功，明显的提交一次事务
+            transaction.savepoint_commit(save_id)
+
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'order_id': order_id})
 
 
 class OrderSettlementView(LoginRequiredMixin,View):
